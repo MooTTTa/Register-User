5c814f0e4a335ca9d2b5d6f68233191ceeace9d2
Mota <mayconmotamendes@gmail.com>
2023-09-09 15:36:42 -0300

    Crete repo

---
+++ src/main/java/com/registeruser/RegisterUser/RegisterUserApplication.java
@@ -0,0 +1,13 @@
+package com.registeruser.RegisterUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterUserApplication.class, args);
+	}
+
+}
